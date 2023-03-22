df0ce1ada603b7f7a44cba2648f0b0f1430fbc73
leocavalcante <leonardo.cavalcante@picpay.com>
2023-03-22 13:05:50 -0300

    :tada:

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 Building a 150 KB web blog in V &amp; SQLite
 
 - from https://github.com/vlang/v/tree/master/tutorials/building_a_simple_web_blog_with_vweb
+
+```shell
+curl http://localhost:8080
+```
+
+```shell
+curl -XPOST -d'{"title":"V Rocks!", "text": "Learning V"}' http://localhost:8080
+```
--- src/main.v
+++ src/main.v
@@ -1,17 +1,60 @@
 module main
 
+import db.sqlite
 import vweb
+import json
 
 struct App {
 	vweb.Context
+pub mut:
+	db sqlite.DB
+}
+
+struct Err {
+	error   bool
+	message string
 }
 
 fn main() {
-	app := App{}
+	app := App{
+		db: sqlite.connect(':memory:') or { panic(err) }
+	}
+
+	sql app.db {
+		create table Article
+	}
+
+	article := Article{
+		title: 'Hello, World!'
+		text: 'V is great.'
+	}
+
+	sql app.db {
+		insert article into Article
+	}
+
 	vweb.run(app, 8080)
 }
 
-['/index']
-pub fn (mut app App) index() vweb.Result {
-	return app.text('Hello, World!')
+['/'; get]
+pub fn (mut app App) list_articles() vweb.Result {
+	articles := app.find_all_articles()
+	return app.json(articles)
+}
+
+['/'; post]
+pub fn (mut app App) create_article() vweb.Result {
+	article := json.decode(Article, app.req.data) or {
+		app.set_status(422, '')
+		return app.json(Err{
+			error: true
+			message: 'Could not decode article'
+		})
+	}
+
+	sql app.db {
+		insert article into Article
+	}
+
+	return app.json(article)
 }
